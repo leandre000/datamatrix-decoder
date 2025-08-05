f5471d75a94d73898a475366e727b6980394a035
Shema Leandre <iamshemaleandre@gmail.com>
2025-08-05 20:32:04 +0200

    feat: create package structure

--- datamatrix_decoder/__init__.py
+++ datamatrix_decoder/__init__.py
@@ -1,21 +1,3 @@
-"""DataMatrix Decoder - Professional barcode decoding library."""
+__version__ = '0.1.0'
+__author__ = 'Leandre'
 
-__version__ = "1.0.0"
-__author__ = "Leandre"
-
-from datamatrix_decoder.core.decoder import DataMatrixDecoder, BarcodeDecoder
-from datamatrix_decoder.core.exceptions import (
-    DecoderError,
-    ImageLoadError,
-    DecodeError,
-    UnsupportedFormatError,
-)
-
-__all__ = [
-    "DataMatrixDecoder",
-    "BarcodeDecoder",
-    "DecoderError",
-    "ImageLoadError",
-    "DecodeError",
-    "UnsupportedFormatError",
-]
