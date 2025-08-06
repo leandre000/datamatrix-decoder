b89de3b1f6fa9e2a6515f8f0d2e7879cb28b712b
Shema Leandre <iamshemaleandre@gmail.com>
2025-08-06 15:44:04 +0200

    feat: add custom exceptions

--- datamatrix_decoder/core/exceptions.py
+++ datamatrix_decoder/core/exceptions.py
@@ -1,26 +1,3 @@
-"""Custom exceptions for the decoder."""
-
-
 class DecoderError(Exception):
-    """Base exception for decoder errors.""" 
     pass
 
-
-class ImageLoadError(DecoderError):
-    """Raised when image cannot be loaded.""" 
-    pass
-
-
-class DecodeError(DecoderError):
-    """Raised when decoding fails.""" 
-    pass
-
-
-class UnsupportedFormatError(DecoderError):
-    """Raised when barcode format is not supported.""" 
-    pass
-
-
-class ConfigurationError(DecoderError):
-    """Raised when configuration is invalid.""" 
-    pass
