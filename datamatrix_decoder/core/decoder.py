5543c32cc0893f575021091a55a5878d41a75b81
Shema Leandre <iamshemaleandre@gmail.com>
2025-08-09 10:56:04 +0200

    feat: add image loading functionality

--- datamatrix_decoder/core/decoder.py
+++ datamatrix_decoder/core/decoder.py
@@ -1,6 +1,9 @@
-import logging
+from PIL import Image
 
 class DataMatrixDecoder:
     def __init__(self):
         self.timeout = 30
+    
+    def load_image(self, path):
+        return Image.open(path)
 
