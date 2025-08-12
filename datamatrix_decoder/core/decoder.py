00c0d58a60b76ee8f045400d4577e1941b4584ed
Shema Leandre <iamshemaleandre@gmail.com>
2025-08-12 15:44:04 +0200

    feat: add Aztec code support

--- datamatrix_decoder/core/decoder.py
+++ datamatrix_decoder/core/decoder.py
@@ -1,7 +1,3 @@
-from pyzbar import pyzbar
-
 class BarcodeDecoder:
-    SUPPORTED_FORMATS = ['qrcode', 'datamatrix']
-    def decode_image(self, path):
-        pass
+    SUPPORTED_FORMATS = ['qrcode', 'datamatrix', 'aztec']
 
