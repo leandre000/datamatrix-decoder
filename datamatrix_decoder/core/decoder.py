461c2fef70623b31ff3cd2327efdd39151384f0b
Shema Leandre <iamshemaleandre@gmail.com>
2025-08-14 15:44:04 +0200

    fix: correct barcode type detection

--- datamatrix_decoder/core/decoder.py
+++ datamatrix_decoder/core/decoder.py
@@ -1,6 +1,4 @@
-class BarcodeDecoder:
-    def _validate_formats(self):
-        for fmt in self.formats:
-            if fmt not in self.SUPPORTED_FORMATS:
-                raise ValueError(fmt)
+def decode_image(self, path):
+    decoded = pyzbar.decode(Image.open(path))
+    return [obj.type for obj in decoded]
 
